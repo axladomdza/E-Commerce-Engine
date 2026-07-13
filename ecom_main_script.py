86b1e7d411ecddd6b26ff338a58063b3c24ba4fb
Axl Adomdza <elikojoaklade@gmail.com>
2026-07-12 20:09:58 -0400

    in-file project descr

--- ecom_main_script.py
+++ ecom_main_script.py
@@ -1,2 +1,6 @@
-#FFor this project we are creating an E-Commerce Engine that trakcs orders from customers and also keeps an inventory (dictionary) in check
-# 
+# For this project we are creating an E-Commerce Engine that trakcs orders from customers and also keeps an inventory (dictionary) in check
+# Dictonaries will be used for the inventory
+# Lists will be used for incoming orders
+# Sets will be used for unique customer names 
+# Functions will be used for the actions of the customers
+
